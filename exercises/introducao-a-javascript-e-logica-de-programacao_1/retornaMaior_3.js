5336b434593505872939cdb4e6f3460083aa6bd5
Jonathan Souza <souzajonathan570@gmail.com>
2021-02-24 19:17:00 -0300

    adicionado retornarValorMaior.js e retornaMaior_3.js

---
+++ exercises/introducao-a-javascript-e-logica-de-programacao_1/retornaMaior_3.js
@@ -0,0 +1,13 @@
+let a = 10;
+let b = 29;
+let c = 5;
+
+if (a > b && a > c) {
+  return console.log(a);
+}
+else if (b > a && b > c) {
+  return console.log(b);
+}
+else {
+  return console.log(c);
+}
---
+++ exercises/introducao-a-javascript-e-logica-de-programacao_1/retornarValorMaior.js
@@ -0,0 +1,8 @@
+let a = 10;
+let b = 29;
+
+if (a > b) {
+  return console.log(a);
+} else {
+  return console.log(b);
+}
