eb6930f47e19172e2bcb6bc7db77e892d89860bd
Jonathan Souza <souzajonathan570@gmail.com>
2021-02-24 21:32:39 -0300

    alterando o xadrez.js (item 6)

--- exercises/introducao-a-javascript-e-logica-de-programacao_1/xadrez.js
+++ exercises/introducao-a-javascript-e-logica-de-programacao_1/xadrez.js
@@ -3,27 +3,27 @@ let minisculo = pecaXadrez.toLowerCase();
 
 switch (minisculo) {
   case 'torre':
-    return console.log('Move-se em linha reta, tanto na vertical quanto na horizontal, quantas casas quiser');
+    return console.log('torre -> horizontal ou vertical');
     break;
   
   case 'cavalo':
-    return console.log('É a única peça que pode saltar sobre as outras peças do tabuleiro, sejam elas amigas ou inimigas. O movimento executado pelo Cavalo é conhecido por “um-dois” ou “em L”. Ele pode andar duas casas na horizontal e uma na vertical, ou duas na vertical e uma na horizontal, uma na horizontal e duas na vertical, e assim por diante. Quando a casa de saída do Cavalo for escura, a casa de chegada será clara, e vice-versa');
+    return console.log('cavalo -> horizontal ou vertical');
     break;
 
   case 'bispo':
-    return console.log('Move-se na diagonal, quantas casas quiser');
+    return console.log('bispo -> diagonal');
     break;
 
   case 'rei':
-    return console.log('mover-se em qualquer direção, porém apenas uma casa por vez, como indicam os pontos vermelhos na figura abaixo');
+    return console.log('rei -> qualquer direção');
     break;
 
   case 'rainha':
-    return console.log('Assim como o Rei, pode mover-se em qualquer direção (vertical, horizontal e diagonal), porém quantas casas quiser, desde que estejam livres');
+    return console.log('rainha -> quaquer direção');
     break;
 
   case 'peão':
-    return console.log('Move-se sempre uma casa para frente, exceto no primeiro movimento, quando pode mover-se duas casas. O peão é a única peça que não pode retroceder, e também a única que efetua a captura com um movimento diferente do utilizado para avançar no tabuleiro. O peão pode capturar as peças que estejam uma fileira acima, mas nas colunas adjacentes a sua');
+    return console.log('peão -> vertical');
     break;
 
   default:
