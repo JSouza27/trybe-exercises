9bc18ab4f1dd7374a6bb570dcaf81cb7ae0c11d4
Jonathan Souza <souzajonathan570@gmail.com>
2021-03-26 17:35:31 -0300

    add teste da funçao greetPeople

--- Bloco  7/7.3/tempCodeRunnerFile.js
+++ Bloco  7/7.3/tempCodeRunnerFile.js
@@ -1,16 +1,1 @@
-const assert = require('assert');
-
-const greetPeople = (people) => {
-  let greeting = [];
-
-  for (const person in people) {
-   greeting.push( `Hello ${people[person]}`);
-  }
-  return greeting;
-};
-
-// assert.strictEqual(typeof greetPeople, 'function');
-
-const parameter = ['Irina', 'Ashleigh', 'Elsa'];
-const result = ['Hello Irina', 'Hello Ashleigh', 'Hello Elsa'];
-greetPeople(parameter);
+++num
