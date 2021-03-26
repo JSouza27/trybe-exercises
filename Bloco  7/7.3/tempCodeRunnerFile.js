bb95b862288250b908eee3207ad360d764a4678d
Jonathan Souza <souzajonathan570@gmail.com>
2021-03-26 17:42:38 -0300

    add teste da funçao greaterThanTen

--- Bloco  7/7.3/tempCodeRunnerFile.js
+++ Bloco  7/7.3/tempCodeRunnerFile.js
@@ -1,1 +1,13 @@
-++num
+const assert = require('assert');
+
+const greaterThanTen = (array) => {
+  let results = 0;
+  for (let index = 0; index < array.length; index += 1) {
+    if (array[index] > 10) {
+      results += array[index];
+    }
+  }
+  return results;
+};
+
+assert.strictEqual(typeof greaterThanTen, 'function');
